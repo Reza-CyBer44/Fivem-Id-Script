750dd97b8b0085a473347c0f3afcd3d0f2618b67
Reza-CyBer44 <64094365+Reza-CyBer44@users.noreply.github.com>
2020-10-28 08:25:05 +0330

    Update Client.lua

--- idscript/Client.lua
+++ idscript/Client.lua
@@ -1,5 +1,2 @@
-RegisterCommand('id', function(source, args)
-    TriggerEvent('chatMessage', "[" .. "ID" .. "]", {255, 0, 0},
-                 '^1 Your Server ID is ^2 ' .. tostring(GetPlayerServerId(source)))
-end, false)
+
 TriggerEvent('chat:addSuggestion', '/id', 'Check Your Server ID', {})
